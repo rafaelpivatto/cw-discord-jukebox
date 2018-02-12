3e85ea3f894425f1b29b5b9e79c08d86395176e5
Rafael Pivatto <rafael.pivatto@gmail.com>
2018-02-12 02:10:47 -0200

    adding express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 require('discord.js');
+
 const express = require('express');
 const Commando = require('discord.js-commando');
 const logger = require('heroku-logger');
@@ -11,17 +12,25 @@ const client = new Commando.Client({
     unknownCommandResponse: false,
 });
 
-logger.info(logName + ' Initializing bot');
+logger.info(logName + ' =========>Initializing bot');
 
 client.registry.registerGroup('music');
 client.registry.registerCommandsIn(__dirname + '/commands');
 
 client.login(process.env.BOT_KEY);
 
+var server = app.listen(process.env.PORT || 5000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    logger.info('Server listening at ' + host + ':' + port);
+});
+
 app.all('/', function (req, res) {
     res.status(200).send('Ok!');
 });
 
 client.on('ready', (arg) => {
-    logger.info(logName + ' Bot started');
+    setTimeout(() => {
+        logger.info(logName + ' =========> Bot started');
+    }, 3000);
 });
